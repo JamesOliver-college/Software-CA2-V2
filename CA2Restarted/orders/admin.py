aab19903d26561e24de3fec73cc6d4b9d5b5245d
X00152764 James Posse Oliver <X00152764@computing.stu.it-tallaght.ie>
2019-12-03 13:25:22 +0000

    Fixed a stupid error

--- CA2Restarted/orders/admin.py
+++ CA2Restarted/orders/admin.py
@@ -7,12 +7,12 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(
+        reverse('orders:admin_order_detail', args=[obj.id])))
+        
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
-
-def order_detail(obj):
-    return mark_safe('<a href="{}">View</a>'.format(
-        reverse('orders:admin_order_detail', args=[obj.id])))
