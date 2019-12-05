fee6db648bfec867f4be916011d341312551dca8
X00156201 Ngury Pinho <X00156201@computing.stu.it-tallaght.ie>
2019-12-05 13:34:23 +0000

    Update to emailapp

--- CA2Restarted/emailapp/views.py
+++ CA2Restarted/emailapp/views.py
@@ -1,1 +1,17 @@
 from django.shortcuts import render
+
+
+
+from .email import Email
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            user_email = form.cleaned_data['email']
+            signup_user = User.objects.get(name='Customer')
+            customer_group = Group.objects.get(name='Customer')
+            customer_group.user_set.add(signup_user)
+            Email.sendSignUpConfirmation(request, username, user_email)
